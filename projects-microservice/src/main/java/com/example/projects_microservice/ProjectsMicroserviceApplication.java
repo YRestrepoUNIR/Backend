b2dca79cbf3a97a6385d2c22e3ee11f9eaf962fc
YuliRestrepo <yulissatatiana@gmail.com>
2024-11-24 19:24:12 -0500

    feat: add estructure project microservice

---
+++ projects-microservice/src/main/java/com/example/projects_microservice/ProjectsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.projects_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsMicroserviceApplication.class, args);
+	}
+
+}
